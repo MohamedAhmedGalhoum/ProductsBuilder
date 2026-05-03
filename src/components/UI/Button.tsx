7db6fd46a423a58be13ba16e52024d87991b2fba
Mohamed Ahmed Galhoum <mohamed.galhoum778@gmail.com>
2026-05-03 14:07:05 +0200

    Make Buttons More Dynaimc with sizes

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -3,11 +3,12 @@ import {ReactNode, ButtonHTMLAttributes} from "react"
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   ClassName?: string;
+  width?: 'w-fit' | 'w-full';
 }
 
-const Button = ({children, ClassName, ...rest}: IProps) => {
+const Button = ({children, ClassName, width = "w-fit", ...rest}: IProps) => {
   return (
-    <button className={`text-white py-2 px-4 rounded-lg transition ${ClassName || ''}`} {... rest}> 
+    <button className={`text-white py-2 px-4 rounded-md transition ${width} ${ClassName || ''}`} {... rest}> 
       {children}
     </button>
   )
